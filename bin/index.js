f27cc14ae8d0630defe07b82ccfeb8d0bcf752f7
Phuc Dang <phucdangb1400718@gmail.com>
2026-02-03 18:45:46 +0700

    bug: fix issue generate docker-compose file

--- bin/index.js
+++ bin/index.js
@@ -4,13 +4,23 @@ import { Command } from 'commander';
 import chalk from 'chalk';
 import { getProjectDetails } from '../lib/prompts.js';
 import { generateProject } from '../lib/generator.js';
+import { readFileSync } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pkg = JSON.parse(readFileSync(join(__dirname, '../package.json'), 'utf-8'));
 
 const program = new Command();
 
 program
     .name('nodejs-quickstart')
-    .description('CLI to scaffold Node.js microservices')
-    .version('1.0.0');
+    .description('🚀 CLI to scaffold production-ready Node.js microservices.\n\nGenerates projects with:\n- MVC or Clean Architecture\n- REST or Kafka\n- MySQL or PostgreSQL\n- Docker & Flyway support')
+    .version(pkg.version, '-v, --version', 'Output the current version')
+    .addHelpText('after', `
+\n${chalk.yellow('Example:')}
+  $ nodejs-quickstart init   ${chalk.gray('# Start the interactive setup')}
+`);
 
 program
     .command('init')
