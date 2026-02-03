7ad7827316b2609db7f7825f42b7450311cc1938
Phuc Dang <phucdangb1400718@gmail.com>
2026-02-03 21:00:31 +0700

    bug fix generate connect to db

--- templates/clean-architecture/ts/src/interfaces/controllers/userController.ts
+++ templates/clean-architecture/ts/src/interfaces/controllers/userController.ts
@@ -3,12 +3,12 @@ import UserRepository from '../../infrastructure/repositories/userRepository';
 import CreateUser from '../../usecases/createUser';
 
 const userRepository = new UserRepository();
-const createUser = CreateUser(userRepository);
+const createUserUseCase = CreateUser(userRepository);
 
 export const createUser = async (req: Request, res: Response) => {
     try {
         const { name, email } = req.body;
-        const user = await createUser(name, email);
+        const user = await createUserUseCase(name, email);
         res.status(201).json(user);
     } catch (error: any) {
         res.status(500).json({ error: error.message });
