137287586c21472ac0eb9e79a0371a76fe3b807b
Phuc Dang <phucdangb1400718@gmail.com>
2026-02-05 22:05:21 +0700

    Fix  case-sensitivity

--- templates/clean-architecture/ts/src/infrastructure/repositories/userRepository.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../../domain/user';
-
-export class UserRepository {
-    async save(user: User) {
-        console.log('Saving user to DB:', user);
-        // Implementation for DB (pg/mysql) would go here
-        return { ...user, id: 123 };
-    }
-}
