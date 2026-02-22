a11ba90540dc192811645380692039a242e11e1d
Phuc Dang <phucdangb1400718@gmail.com>
2026-02-22 17:28:28 +0700

    Fix issue import file cache

--- scripts/find-index.js
+++
@@ -1,9 +0,0 @@
-
-import { combinations } from './lib/validation-core.js';
-
-const target1 = 'test_clean_javascript_mysql_restapis_memorycache';
-const target2 = 'test_mvc_typescript_none_mongodb_restapis_redis';
-
-console.log('Target 1 (' + target1 + '): ' + combinations.findIndex(c => c.projectName === target1));
-console.log('Target 2 (' + target2 + '): ' + combinations.findIndex(c => c.projectName === target2));
-
