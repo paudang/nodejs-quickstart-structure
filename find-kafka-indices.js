899c9ea0766dc2361da688222c731334c6980a26
Phuc Dang <phucdangb1400718@gmail.com>
2026-03-16 08:23:15 +0700

    Enhance import and unit test

--- find-kafka-indices.js
+++
@@ -1,7 +0,0 @@
-import { combinations } from './scripts/lib/validation-core.js';
-
-combinations.forEach((c, i) => {
-    if (c.communication === 'Kafka') {
-        console.log(`${i}: ${c.projectName} (${c.architecture}, ${c.language})`);
-    }
-});
