22cda584d58319af9628bbc52b0b4b984318474d
PauDang <phucdangb1400718@gmail.com>
2026-04-02 16:10:35 +0700

    icon in the mobile view

--- docs/.vitepress/theme/components/SimulationTree.vue
+++ docs/.vitepress/theme/components/SimulationTree.vue
@@ -505,7 +505,7 @@
         <div class="tree-item" style="--depth: 1">
           <svg class="tree-item-icon icon-file-doc" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline></svg>
           .snyk
-          <span class="tree-comment"># 🔒 Security policy for vulnerability scanning</span>
+          <span class="tree-comment"># Security policy for vulnerability scanning</span>
         </div>
       </template>
     </div>
